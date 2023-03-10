1b3d628575f5beb8dc57d40fafb08adfd32d8d2b
suman jha <suman.j@simformsolutions.com>
2023-03-10 17:24:12 +0530

    added practical 1 part b

---
+++ practical 1 -part b/DebuggingApplication/DebuggingApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DebuggingApplication
+{
+    public class DebuggingApplication
+    {
+        public  void Debug()
+        {
+
+            string userResponse = Console.ReadLine();
+
+            if (Convert.ToBoolean(int.Parse(userResponse)))
+            {
+
+                Console.WriteLine("The entered value is a integer");
+
+            }
+
+        }
+
+        public static void Main(string[] args)
+        {
+            DebuggingApplication debug = new();
+            debug.Debug();
+        }
+
+    }
+}
