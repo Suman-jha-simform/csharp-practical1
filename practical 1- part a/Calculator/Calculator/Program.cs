12d6be043c9a02e7076a3dfbcd534714ca98dddd
suman jha <suman.j@simformsolutions.com>
2023-03-13 12:55:11 +0530

    Update commnets in part a

--- practical 1- part a/Calculator/Calculator/Program.cs
+++ practical 1- part a/Calculator/Calculator/Program.cs
@@ -15,13 +15,19 @@ namespace CalculatorApplication
 
                 double userNumber1;
                 double userNumber2;
-                                                                                            // Taking user input 
+
+                // Taking user input
                 Console.Write("\nNumber 1 : ");
                 bool isintNumber1 = double.TryParse(Console.ReadLine(), out userNumber1);
                 Console.Write("Number 2 : ");
                 bool isintNumber2 = double.TryParse(Console.ReadLine(), out userNumber2);
 
-                if (isintNumber1 && isintNumber2)                                          // checking if user input is valid
+                // checking if user input is valid
+                if(isintNumber2 && userNumber2 == 0)
+                {
+                    Console.WriteLine("Second number cannot be zero.");
+                }
+                else if (isintNumber1 && isintNumber2)                                          
                 {
                     Console.WriteLine("\nAddition of Number is : {0}", userNumber1 + userNumber2);
                     Console.WriteLine("Subtraction of Number is : {0}", userNumber1 - userNumber2);
@@ -41,9 +47,11 @@ namespace CalculatorApplication
                     {
                         Console.WriteLine("please enter a valid choice !!! ");
                     }
-                } while (userChoice.ToLower() != "yes" && userChoice.ToLower() != "no");         // loop until user enters a valid choice
+                    // loop until user enters a valid choice
+                } while (userChoice.ToLower() != "yes" && userChoice.ToLower() != "no");
 
-            } while (userChoice.ToLower() == "yes");        // if userchoice is yes continue otherwise exit
+                // if userchoice is yes continue otherwise exit
+            } while (userChoice.ToLower() == "yes");        
 
 
         }
